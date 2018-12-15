d668dd53f7af3e60d3a031c63d7bd8a896c61974
Fredrik Walterson <fredrik.walterson@gmail.com>
2018-12-15 15:03:02 +0100

    Unfinished AnimationListener

--- ReactionGame/app/src/main/java/bigbois/reactiontest/ReactionTest.java
+++ ReactionGame/app/src/main/java/bigbois/reactiontest/ReactionTest.java
@@ -4,21 +4,41 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SantaListener implements Animation.AnimationListener {
+@Override
+public void onAnimationStart(Animation animation) {
+
+        }
+
+@Override
+public void onAnimationEnd(Animation animation) {
+        onAnimationCompleted();
+        }
+
+@Override
+public void onAnimationRepeat(Animation animation) {
+
+        }
+}
 
 public class ReactionTest extends AppCompatActivity {
 
     EditText playerName;
     Button startButton;
     ImageView santaImg;
+    TextView hitSanta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reaction_test);
+        setContentView(R.layout.start_reaction_game);
 
         playerName = findViewById(R.id.playerText);
 
@@ -35,8 +55,22 @@ public class ReactionTest extends AppCompatActivity {
         RotateAnimation rotSanta = new RotateAnimation(santaImg.getRotation(), santaImg.getRotation() + 360, santaImg.getWidth()/2, santaImg.getHeight()/2);
         rotSanta.setDuration(1000);
         rotSanta.setFillAfter(true);
-        santaImg.startAnimation(rotSanta);
 
-        System.out.println(playerName.getText().toString());
+        SantaListener santaListener = new SantaListener();
+        rotSanta.setAnimationListener(santaListener);
+
+        santaImg.startAnimation(rotSanta);
+    }
+
+    public void onAnimationCompleted() throws InterruptedException
+    {
+        setContentView(R.layout.run_reaction_game);
+        hitSanta = findViewById(R.id.instructionTxt);
+        Thread.sleep(1000);
+        hitSanta.setVisibility(View.VISIBLE);
+        Thread.sleep(3000);
+        setContentView(R.layout.start_reaction_game);
     }
 }
+
+
