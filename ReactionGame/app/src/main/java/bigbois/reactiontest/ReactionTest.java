e7e47f0b717ebf1b39c3ebec25867af6fc80733f
Robert Eriksson <Robert.Eriksson@netent.com>
2018-12-15 15:57:34 +0100

    Add timeout before switching scene

--- ReactionGame/app/src/main/java/bigbois/reactiontest/ReactionTest.java
+++ ReactionGame/app/src/main/java/bigbois/reactiontest/ReactionTest.java
@@ -1,6 +1,7 @@
 package bigbois.reactiontest;
 
 import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -34,28 +35,30 @@ public class ReactionTest extends AppCompatActivity {
     {
         MediaPlayer santaSound = MediaPlayer.create(ReactionTest.this, R.raw.santa_sound);
         santaSound.start();
-
         RotateAnimation rotSanta = new RotateAnimation(santaImg.getRotation(), santaImg.getRotation() + 360, santaImg.getWidth()/2, santaImg.getHeight()/2);
         rotSanta.setDuration(1000);
         rotSanta.setFillAfter(true);
-
         SantaListener santaListener = new SantaListener(this);
         rotSanta.setAnimationListener(santaListener);
         santaImg.startAnimation(rotSanta);
 
     }
 
-    public void onAnimationCompleted() throws InterruptedException
+    public void onAnimationCompleted()
     {
         System.out.println("onAnimationCompletedStart");
         setContentView(R.layout.run_reaction_game);
-        Thread.sleep(3000);
-        hitSanta = findViewById(R.id.instructionTxt);
-        hitSanta.setVisibility(View.VISIBLE);
-        Thread.sleep(3000);
-        setContentView(R.layout.start_reaction_game);
-        System.out.println("onAnimationCompletedEnd");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                hitSanta = findViewById(R.id.instructionTxt);
+                santaImg = findViewById(R.id.santaImg);
+                hitSanta.setVisibility(View.VISIBLE);
+                santaImg.setVisibility(View.VISIBLE);
+            }
+        }, 3000);
     }
+
 }
 
 
